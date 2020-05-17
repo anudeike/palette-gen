986424af87a9321c8b55e230dd619baa461f3ad0
anudeike <ikechukwuanude@gmail.com>
2020-05-16 19:33:39 -0500

    implemented a version of the relu function and its derivative

--- main.py
+++ main.py
@@ -119,6 +119,157 @@ def plotColorsInSample(sample_unzipped, raw_sample):
     ax.scatter(sample_unzipped[0], sample_unzipped[1], sample_unzipped[2], c = raw_sample/255)
     plt.show()
 
+
+# compute sigmoid nonlinearity
+def sigmoid(x, deriv=False):
+    # if there is a derivative
+    if deriv:
+        return x * (1 - x)
+
+    # else
+    output = 1 / (1 + np.exp(-x))
+    return output
+
+# define the relu function
+def relu(x, deriv = False):
+    # this should work?
+    if deriv:
+        return 1 * (x > 0)
+
+    return x * (x > 0)
+
+# define leaky relu
+def leaky_relu(x):
+    return
+
+# create a function to generate the weights
+def initialize_weights():
+    pass
+
+# much of this code it taken from I am trask
+def network():
+
+    # input dataset
+    X = np.array([[0, 0, 1],
+                  [0, 1, 1],
+                  [1, 0, 1],
+                  [1, 1, 1]])
+
+    # output dataset
+    y = np.array([[0,
+                   1,
+                   1,
+                   0]]).T
+
+    np.random.seed(1)
+
+
+
+    # set the alpha
+    alpha = 10
+
+    # setting the size of the hiddenLayer
+    hidden_size = 3
+
+    # xavier initlization
+    synapse_0 = np.random.randn(X.shape[1], hidden_size) * np.sqrt(1 / (X.shape[1] - 1))
+    synapse_1 = np.random.randn(hidden_size, y.shape[1]) * np.sqrt(1 / (X.shape[1] - 1))
+
+    for j in range(10000):
+
+        # Feed forward through layers 0, 1, and 2
+        layer_0 = X
+        layer_1 = sigmoid(np.dot(layer_0, synapse_0))
+        layer_2 = sigmoid(np.dot(layer_1, synapse_1))
+
+        # how much did we miss the target value?
+        layer_2_error = layer_2 - y
+
+        # print the error after a certain about of iterations
+        if (j % 1000) == 0:
+            print ("Error after " + str(j) + " iterations:" + str(np.mean(np.abs(layer_2_error))))
+
+        # in what direction is the target value?
+        # were we really sure? if so, don't change too much.
+        layer_2_delta = layer_2_error * sigmoid(layer_2, True)
+
+        # how much did each l1 value contribute to the l2 error (according to the weights)?
+        layer_1_error = layer_2_delta.dot(synapse_1.T)
+
+        # in what direction is the target l1?
+        # were we really sure? if so, don't change too much.
+        layer_1_delta = layer_1_error * sigmoid(layer_1, True)
+
+        # gradient descent happens here
+        synapse_1 -= alpha * (layer_1.T.dot(layer_2_delta))
+        synapse_0 -= alpha * (layer_0.T.dot(layer_1_delta))
+
+    print("output after training: ")
+    print(layer_1.shape)
+
+# much of this code it taken from I am trask
+def network_relu(leaky_relu = False):
+
+    # input dataset
+    X = np.array([[0, 0, 1],
+                  [0, 1, 1],
+                  [1, 0, 1],
+                  [1, 1, 1]])
+
+    # output dataset
+    y = np.array([[0,
+                   1,
+                   1,
+                   0]]).T
+
+    np.random.seed(1)
+
+
+
+    # set the alpha
+    alpha = 10
+
+    # setting the size of the hiddenLayer
+    hidden_size = 3
+
+    # xavier initlization
+    synapse_0 = np.random.randn(X.shape[1], hidden_size) * np.sqrt(1 / (X.shape[1] - 1))
+    synapse_1 = np.random.randn(hidden_size, y.shape[1]) * np.sqrt(1 / (X.shape[1] - 1))
+
+    for j in range(10000):
+
+        # Feed forward through layers 0, 1, and 2
+        layer_0 = X
+        layer_1 = sigmoid(np.dot(layer_0, synapse_0))
+        layer_2 = sigmoid(np.dot(layer_1, synapse_1))
+
+        # how much did we miss the target value?
+        layer_2_error = layer_2 - y
+
+        # print the error after a certain about of iterations
+        if (j % 1000) == 0:
+            print ("Error after " + str(j) + " iterations:" + str(np.mean(np.abs(layer_2_error))))
+
+        # in what direction is the target value?
+        # were we really sure? if so, don't change too much.
+        layer_2_delta = layer_2_error * sigmoid(layer_2, True)
+
+        # how much did each l1 value contribute to the l2 error (according to the weights)?
+        layer_1_error = layer_2_delta.dot(synapse_1.T)
+
+        # in what direction is the target l1?
+        # were we really sure? if so, don't change too much.
+        layer_1_delta = layer_1_error * sigmoid(layer_1, True)
+
+        # gradient descent happens here
+        synapse_1 -= alpha * (layer_1.T.dot(layer_2_delta))
+        synapse_0 -= alpha * (layer_0.T.dot(layer_1_delta))
+
+    print("output after training: ")
+    print(layer_1.shape)
+
+
+
 def main():
   # generate a bunch of test palettes with specified means and std_dev
   # details of the distributions
@@ -143,6 +294,9 @@ def main():
 
   # (X, Y)
   print(test_pals[1])
+
+  # call the network function
+  network()
 
 
 
