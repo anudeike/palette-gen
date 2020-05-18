ec8b5e861e3641a24a161fe4f9033873c8773597
anudeike <ikechukwuanude@gmail.com>
2020-05-18 18:15:00 -0500

    installed the right colorthief directory and can now get colors

--- colorData/index.js
+++ colorData/index.js
@@ -1,5 +1,12 @@
 // this file gets all the color information from the pictures that I chose
-var ColorThief = require('color-thief')
+var ColorThief = require('colorthief');
+var fs = require("fs");
 
-// instance
-var colorThief = new ColorThief();
+// the color thief module only needs a path to the photos
+
+// get the colors
+ColorThief.getColor('sunset_example.png').then(c => {
+    console.log(c);
+}).catch(e => {
+    console.log(e);
+})
